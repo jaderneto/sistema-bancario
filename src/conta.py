47ddbd80bce658149d65554e66b8d5d58b8352ae
Jader Gomes <jader.netoo@hotmail.com>
2022-04-11 00:36:42 -0300

    Inclusão do metodo de mostrar o extrato bancario

--- src/conta.py
+++ src/conta.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 
 class Conta:
-    def __init__(self, creating_at, owner):
+    def __init__(self, owner):
         self.value = 0
         self.moviments = []
-        self.creating_at = creating_at
+        self.creation_at = datetime.now()
         self.owner = owner
 
     def deposit(self, amount):
@@ -15,3 +15,13 @@ class Conta:
     def withdraw(self, amount):
         self.value -= amount
         self.moviments.append(('withdraw', datetime.now(), amount))
+
+    def getinfo(self):
+        print('Conta corrente do %s possui um saldo de R$ %.2f' % (self.owner.name, self.value))
+
+    def show_moviments(self):
+        print('----------------------------- Extrato bancario ---------------------------------')
+        for i in range(len(self.moviments)):
+            print('Tipo: %s | Valor movimentado: R$ %.2f | Data: %s' % (self.moviments[i][0], self.moviments[i][2], self.moviments[i][1]))
+
+        print('--------------------------------------------------------------------------------')
--- teste.py
+++ teste.py
@@ -1,15 +1,14 @@
 from src.conta import *
 from src.person import *
-from datetime import datetime
 
 p1 = Person('Jader', '05825399356', 'Male')
-c1 = Conta(datetime.now(), p1)
+c1 = Conta(p1)
 c1.deposit(100)
 c1.deposit(300)
 
 p2 = Person('Taiane', '54399235534', 'Female')
-c2 = Conta(datetime.now(), p2)
+c2 = Conta(p2)
 c2.deposit(500)
 
-print(c1.owner.name + ': ', c1.value)
-print(c2.owner.name + ': ', c2.value)
+c1.getinfo()
+c1.show_moviments()
